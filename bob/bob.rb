e2e2c8e79041ed3651365a1d939c6f9f7bea474a
Jonathan Cabezas <jonathan.cabezas@getapp.com>
2018-04-23 15:50:37 +0200

    bob

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,5 +1,12 @@
+# Bob exercise
 class Bob
   def self.hey(remark)
+    case
+    when remark !~ /[a-z]/ && remark =~ /[A-Z]/ then 'Whoa, chill out!'
+    when remark.strip.end_with?('?') then 'Sure.'
+    when remark =~ /\A\s*\z/ then 'Fine. Be that way!'
+    else 'Whatever.'
+    end
   end
 end
 
